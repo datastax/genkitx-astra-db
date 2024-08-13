15a15bbc7001919878d740b22c88bd1a1d01e971
Phil Nash <philnash@gmail.com>
2024-08-13 15:06:39 +1000

    An Astra DB retriever and indexer for Genkit

---
+++ README.md
@@ -0,0 +1,122 @@
+# Astra DB Plugin for Genkit
+
+This plugin provides a [Astra DB](https://docs.datastax.com/en/astra-db-serverless/index.html) retriever and indexer for [Genkit](https://firebase.google.com/docs/genkit).
+
+## Installation
+
+```sh
+npm i genkitx-astra-db
+```
+
+## Configuration
+
+To use the Astra DB plugin, specify it when you call `configureGenkit()`.
+
+```typescript
+import { astraDB } from "genkitx-astra-db";
+
+configureGenkit({
+  plugins: [
+    astraDB([
+      {
+        clientParams: {
+          applicationToken: ASTRA_DB_APPLICATION_TOKEN!,
+          apiEndpoint: ASTRA_DB_API_ENDPOINT!,
+          namespace: "default_keyspace",
+        },
+        collectionName: "your_collection_name",
+        embedder: textEmbeddingGecko001,
+      },
+    ]),
+  ],
+});
+```
+
+### Client Parameters
+
+You will need an Application Token and API Endpoint from Astra DB. You can either provide them through the `clientParams` object or by setting the environment variables `ASTRA_DB_APPLICATION_TOKEN` and `ASTRA_DB_API_ENDPOINT`.
+
+If you are using the default namespace, you do not need to pass it as config.
+
+### Options
+
+* `collectionName`: You need to provide a collection name that matches a collection in the database accessed at the API endpoint
+* `embedder`: You need to provide an embedder, like Google's `textEmbeddingGecko001`. Ensure that you have set up your collection with the correct number of dimensions for the embedder that you are using
+* `embedderOptions`: If the embedder takes extra options you can provide them
+
+## Usage
+
+Import the indexer and retriever references like so:
+
+```typescript
+import { astraDBIndexerRef, astraDBRetrieverRef } from "genkitx-astra-db";
+```
+
+Then get a reference using the `collectionName` and an optional `displayName` and pass the relevant references to the Genkit functions `index()` or `retrieve()`.
+
+### Indexer
+
+```typescript
+export const astraDBIndexer = astraDBIndexerRef({
+  collectionName: "your_collection_name"
+});
+
+await index({
+  indexer: astraDBIndexer,
+  documents,
+});
+```
+
+### Retriever
+
+```typescript
+export const astraDBRetriever = astraDBRetrieverRef({
+  collectionName: "your_collection_name"
+});
+
+await retrieve({
+  retriever: astraDBRetriever,
+  query,
+})
+```
+
+#### Options
+
+You can pass options to `retrieve()` that will affect the retriever. The available options are:
+
+* `k`: The number of documents to return from the retriever. The default is 5.
+* `filter`: A `Filter` as defined by the [Astra DB library](https://docs.datastax.com/en/astra-api-docs/_attachments/typescript-client/types/Filter.html). See below for how to use a filter
+
+#### Advanced usage
+
+If you want to perform a vector search with additional filtering (hybrid search) you can pass a schema type to `astraDBRetrieverRef`. For example:
+
+```typescript
+type Schema = {
+  _id: string;
+  text: string;
+  score: number;
+};
+
+export const astraDBRetriever = astraDBRetrieverRef<Schema>({
+  collectionName: "your_collection_name"
+});
+
+await retrieve({
+  retriever: astraDBRetriever,
+  query,
+  options: { 
+    filter: {
+      score: { $gt: 75 }
+    }
+  }
+});
+```
+
+You can find the [operators that you can use in filters in the Astra DB documentation](https://docs.datastax.com/en/astra-db-serverless/api-reference/overview.html#operators).
+
+If you don't provide a schema type, you can still filter but you won't get type-checking on the filtering options.
+
+## Further information
+
+For more on using indexers and retrievers with Genkit check out the documentation on [Retrieval-Augmented Generation with Genkit](https://firebase.google.com/docs/genkit/rag).
---
+++ src/index.ts
@@ -0,0 +1,193 @@
+import { embed, EmbedderArgument } from "@genkit-ai/ai/embedder";
+import {
+  CommonRetrieverOptionsSchema,
+  defineIndexer,
+  defineRetriever,
+  indexerRef,
+  retrieverRef,
+} from "@genkit-ai/ai/retriever";
+import { genkitPlugin, PluginProvider } from "@genkit-ai/core";
+import { Md5 } from "ts-md5";
+import * as z from "zod";
+import { DataAPIClient, Filter, SomeDoc } from "@datastax/astra-db-ts";
+
+type AstraDBClientOptions = {
+  applicationToken: string;
+  apiEndpoint: string;
+  namespace?: string;
+};
+
+const PLUGIN_NAME = "astradb";
+const DEFAULT_NAMESPACE = "default_keyspace";
+
+const createAstraDBRetrieverOptionsSchema = <Schema extends SomeDoc>() =>
+  CommonRetrieverOptionsSchema.extend({
+    filter: z.custom<Filter<Schema>>().optional(),
+  });
+
+const AstraDBIndexerOptionsSchema = z.object({});
+
+export const astraDBRetrieverRef = <Schema extends SomeDoc>(params: {
+  collectionName: string;
+  displayName?: string;
+}) => {
+  return retrieverRef({
+    name: `${PLUGIN_NAME}/${params.collectionName}`,
+    info: {
+      label: params.displayName ?? `Astra DB - ${params.collectionName}`,
+    },
+    configSchema: createAstraDBRetrieverOptionsSchema<Schema>(),
+  });
+};
+
+export const astraDBIndexerRef = (params: {
+  collectionName: string;
+  displayName?: string;
+}) => {
+  return indexerRef({
+    name: `${PLUGIN_NAME}/${params.collectionName}`,
+    info: {
+      label: params.displayName ?? `Astra DB - ${params.collectionName}`,
+    },
+    configSchema: AstraDBIndexerOptionsSchema,
+  });
+};
+
+export function astraDB<EmbedderCustomOptions extends z.ZodTypeAny>(
+  params: {
+    clientParams?: AstraDBClientOptions;
+    collectionName: string;
+    embedder: EmbedderArgument<EmbedderCustomOptions>;
+    embedderOptions?: z.infer<EmbedderCustomOptions>;
+  }[]
+): PluginProvider {
+  const plugin = genkitPlugin(
+    PLUGIN_NAME,
+    async (
+      params: {
+        clientParams?: AstraDBClientOptions;
+        collectionName: string;
+        embedder: EmbedderArgument<EmbedderCustomOptions>;
+        embedderOptions?: z.infer<EmbedderCustomOptions>;
+      }[]
+    ) => ({
+      retrievers: params.map((i) => astraDBRetriever(i)),
+      indexers: params.map((i) => astraDBIndexer(i)),
+    })
+  );
+  return plugin(params);
+}
+
+export function astraDBRetriever<
+  Schema extends SomeDoc,
+  EmbedderCustomOptions extends z.ZodTypeAny
+>(params: {
+  clientParams?: AstraDBClientOptions;
+  collectionName: string;
+  embedder: EmbedderArgument<EmbedderCustomOptions>;
+  embedderOptions?: z.infer<EmbedderCustomOptions>;
+}) {
+  const { collectionName, embedder, embedderOptions } = params;
+  const { applicationToken, apiEndpoint } =
+    params.clientParams ?? getDefaultConfig();
+  const namespace = params.clientParams?.namespace ?? DEFAULT_NAMESPACE;
+
+  const client = new DataAPIClient(applicationToken);
+  const db = client.db(apiEndpoint, { namespace });
+  const collection = db.collection<Schema>(collectionName);
+
+  return defineRetriever(
+    {
+      name: `${PLUGIN_NAME}/${collectionName}`,
+      configSchema: createAstraDBRetrieverOptionsSchema<Schema>().optional(),
+    },
+    async (content, options) => {
+      const embedding = await embed({
+        embedder,
+        content,
+        options: embedderOptions,
+      });
+      const filter = options?.filter || {};
+      const limit = options?.k || 5;
+
+      const cursor = collection.find(filter, {
+        sort: { $vector: embedding },
+        limit,
+      });
+      const results = await cursor.toArray();
+      const documents = results.map((result) => {
+        const { text, id, ...metadata } = result;
+        return { content: [{ text: text }], metadata };
+      });
+      return { documents };
+    }
+  );
+}
+
+export function astraDBIndexer<
+  EmbedderCustomOptions extends z.ZodTypeAny
+>(params: {
+  clientParams?: AstraDBClientOptions;
+  collectionName: string;
+  embedder: EmbedderArgument<EmbedderCustomOptions>;
+  embedderOptions?: z.infer<EmbedderCustomOptions>;
+}) {
+  const { collectionName, embedder, embedderOptions } = {
+    ...params,
+  };
+  const { applicationToken, apiEndpoint } =
+    params.clientParams ?? getDefaultConfig();
+  const namespace = params.clientParams?.namespace ?? DEFAULT_NAMESPACE;
+
+  const client = new DataAPIClient(applicationToken);
+  const db = client.db(apiEndpoint, { namespace });
+  const collection = db.collection(collectionName);
+
+  return defineIndexer(
+    {
+      name: `${PLUGIN_NAME}/${collectionName}`,
+      configSchema: AstraDBIndexerOptionsSchema,
+    },
+    async (docs) => {
+      const embeddings = await Promise.all(
+        docs.map((doc) =>
+          embed({
+            embedder,
+            content: doc,
+            options: embedderOptions,
+          })
+        )
+      );
+
+      const documents = docs.map((doc, i) => ({
+        _id: Md5.hashStr(JSON.stringify(doc)),
+        text: doc.text(),
+        $vector: embeddings[i],
+        ...doc.metadata,
+      }));
+
+      await collection.insertMany(documents);
+    }
+  );
+}
+
+function getDefaultConfig(): AstraDBClientOptions {
+  const maybeApiKey = process.env.ASTRA_DB_APPLICATION_TOKEN;
+  const maybeEndpoint = process.env.ASTRA_DB_API_ENDPOINT;
+  if (!maybeApiKey) {
+    throw new Error(
+      "Please pass in the API key or set ASTRA_DB_APPLICATION_TOKEN environment variable.\n" +
+        "For more details see https://firebase.google.com/docs/genkit/plugins/astraDB"
+    );
+  }
+  if (!maybeEndpoint) {
+    throw new Error(
+      "Please pass in the Astra DB API endpoint or set ASTRA_DB_API_ENDPOINT environment variable.\n" +
+        "For more details see https://firebase.google.com/docs/genkit/plugins/astraDB"
+    );
+  }
+  return {
+    applicationToken: maybeApiKey,
+    apiEndpoint: maybeEndpoint,
+  };
+}
